91ea5794201f34d41aebfdc029c6b2a02a7bfdf3
Sayan Deb <keepimaginingsayan@gmail.com>
2023-03-20 11:24:57 +0530

    Fully working website

--- RandomDice/index.js
+++ RandomDice/index.js
@@ -14,3 +14,12 @@ var imageSelector2 = "images/"+randomDiceImage2;
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src",imageSelector2);
 
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins";
+}
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 wins";
+}
+else {
+    document.querySelector("h1").innerHTML = "It's is a draw";
+}
