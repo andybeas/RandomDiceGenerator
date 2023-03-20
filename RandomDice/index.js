583c4b1e56fd4438faa68cee79bbdd302a1d7ba7
Sayan Deb <keepimaginingsayan@gmail.com>
2023-03-20 10:43:46 +0530

    Both the dice images are changing at random working

--- RandomDice/index.js
+++ RandomDice/index.js
@@ -1,1 +1,16 @@
-var randomNumber
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage1 = "dice" + randomNumber1 + ".png";
+var imageSelector1 = "images/"+randomDiceImage1;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src",imageSelector1);
+
+
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+var imageSelector2 = "images/"+randomDiceImage2;
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src",imageSelector2);
+
